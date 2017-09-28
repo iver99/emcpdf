b0990c71e9f1955f4068f55f4f8fc4c3341c7bd2
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-09-28 13:58:39 +0800

    remove dup files

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/util/DashBoardPageId_1230.java
+++
@@ -1,6 +0,0 @@
-package oracle.sysman.emaas.platform.dashboards.tests.ui.util;
-
-
-public class DashBoardPageId_1230 {
-    public static final String WIDGET_SELECTOR_SEARCH_INPUT_LOCATOR = "//div[@id='widget-selector-search-container']//input[contains(@class, 'searchTxt')]";
-}
