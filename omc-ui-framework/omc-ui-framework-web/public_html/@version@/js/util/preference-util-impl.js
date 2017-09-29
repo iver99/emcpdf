4a6b47847e5f1c9e41290d1cdd3ea13a5db7df4b
Jiangxia Nan <jiangxia.nan@oracle.com>
2017-09-29 03:12:32 -0700

    show/hide federated view in HM according to pref

--- omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/util/preference-util-impl.js
+++ omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/util/preference-util-impl.js
@@ -1,6 +1,6 @@
 
-define(['jquery', 'uifwk/@version@/js/util/ajax-util-impl'],
-function($, ajaxUtilModel)
+define(['knockout', 'jquery', 'uifwk/@version@/js/util/ajax-util-impl'],
+function(ko, $, ajaxUtilModel)
 {
     var ajaxUtil = new ajaxUtilModel();
 
@@ -12,6 +12,70 @@ function($, ajaxUtilModel)
     var PreferenceUtility = function(prefRestApiUrl, requestHeader) {
         this.prefRestApiUrl = prefRestApiUrl;
         this.requestHeader = requestHeader;
+    };
+    
+    /**
+     * Get preference value to decide show or hide an Item in HM root menu. By default, it is hidden.
+     * 
+     * @param {type} key
+     * @param {type} callback
+     * @param {type} async
+     * @returns {undefined}
+     */
+    PreferenceUtility.prototype.getHMItemShowPreference = function(key,successCallback, errorCallback) {
+        var self = this;
+        if(key) {
+            if(window._uifwk && window._uifwk.cachedData && ko.unwrap(window._uifwk.cachedData.preferences)) {
+                var preferences = ko.unwrap(window._uifwk.cachedData.preferences);
+                for(var i in preferences) {
+                    if(preferences[i].key === key) {
+                        callback(preferences[i].value);
+                        return;
+                    }
+                }
+            }
+            
+            //Didn't get preference value for desired key in cache, send request to get
+            if (!window._uifwk) {
+                window._uifwk = {};
+            }
+            if (!window._uifwk.cachedData) {
+                window._uifwk.cachedData = {};
+            }
+            if (!window._uifwk.cachedData["isFetching"+key+"Preference"]) {
+                window._uifwk.cachedData["isFetching"+key+"Preference"] = true;
+                if (!window._uifwk.cachedData.preferences) {
+                    window._uifwk.cachedData.preferences = ko.observableArray();
+                }
+                function doneCallback(data, textStatus, jqXHR) {
+                    window._uifwk.cachedData.preferences.push(data);
+                    window._uifwk.cachedData["isFetching"+key+"Preference"] = false;
+                    successCallback(data.value, textStatus, jqXHR);
+                }
+                var url = self.prefRestApiUrl + "/" + key;
+                ajaxUtil.ajaxWithRetry({type: 'GET', contentType: 'application/json', url: url,
+                    dataType: 'json',
+                    headers: self.requestHeader,
+                    async: true,
+                    success: function (data, textStatus, jqXHR) {
+                        doneCallback(data, textStatus, jqXHR);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.log('Failed to get HM item show preference info!');
+                        window._uifwk.cachedData["isFetching"+key+"Preference"] = false;
+                        if (errorCallback) {
+                            errorCallback(jqXHR, textStatus, errorThrown);
+                        }
+                    }
+                });
+            } else {
+                window._uifwk.cachedData.preferences.subscribe(function (data) {
+                    if (data) {
+                        successCallback(data.value);
+                    }
+                });
+            }
+        }
     };
 
     PreferenceUtility.prototype.getPreferenceValue = function(prefArray, key) {
