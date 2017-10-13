3527f78d1c492e08074d3ddcc647b85e38d1f0b2
hui.li.li@oracle.com <hbli@slc09xqe.us.oracle.com>
2017-10-12 23:16:08 -0700

    update by Cabel's comments

--- dashboards-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/offboard/offboardTenant.java
+++ dashboards-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/offboard/offboardTenant.java
@@ -27,6 +27,8 @@ public class offboardTenant
 	static String tenantid;
 	static String tenantid_2;
 	static String remoteuser;
+	
+	String dashboard_id = "";
 
 	private static final Logger LOGGER = LogManager.getLogger(FavoriteDashboardCRUD.class);
 
@@ -44,7 +46,7 @@ public class offboardTenant
 		remoteuser = ct.getRemoteUser();
 	}
 	
-	@Test
+	@Test(dependsOnMethods = { "createDashboard" })
 	public void offBoardForTenant()
 	{
 		
@@ -87,4 +89,80 @@ public class offboardTenant
 		}
 
 	}
+	
+	@Test
+	public void createDashboard()
+	{
+
+		/* create a dashboard with required field
+		 *
+		 */
+		try {
+			
+
+			String jsonString1 = "{ \"name\":\"Test_Dashboard4\"}";
+			Response res1 = RestAssured
+					.given()
+					.contentType(ContentType.JSON)
+					.log()
+					.everything()
+					.headers("X-USER-IDENTITY-DOMAIN-NAME", tenantid, "X-REMOTE-USER", tenantid + "." + remoteuser,
+							"Authorization", authToken).body(jsonString1).when().post("/dashboards");		
+		
+			Assert.assertTrue(res1.getStatusCode() == 201);
+			
+			dashboard_id = res1.jsonPath().getString("id");
+
+			
+			Response res4 = RestAssured
+					.given()
+					.contentType(ContentType.JSON)
+					.log()
+					.everything()
+					.headers("X-USER-IDENTITY-DOMAIN-NAME", tenantid, "X-REMOTE-USER", tenantid + "." + remoteuser,
+							"Authorization", authToken).when().get("/dashboards/" + dashboard_id);
+			
+			Assert.assertTrue(res4.getStatusCode() == 200);
+			Assert.assertEquals(res4.jsonPath().getString("name"), "Test_Dashboard4");
+			Assert.assertEquals(res4.jsonPath().getString("type"), "NORMAL");
+			Assert.assertEquals(res4.jsonPath().getString("id"), dashboard_id);
+			Assert.assertEquals(res4.jsonPath().getBoolean("systemDashboard"), false);
+			Assert.assertEquals(res4.jsonPath().getString("tiles"), "[]");
+			
+			String jsonString3 = "{ \"name\":\"Test_Dashboard4\"}";
+			Response res3 = RestAssured
+					.given()
+					.contentType(ContentType.JSON)
+					.log()
+					.everything()
+					.headers("X-USER-IDENTITY-DOMAIN-NAME", tenantid, "X-REMOTE-USER", tenantid + "." + remoteuser,
+							"Authorization", authToken).body(jsonString3).when().post("/dashboards");
+			
+			Assert.assertTrue(res3.getStatusCode() == 400);
+			Assert.assertEquals(res3.jsonPath().getString("errorCode"), "10001");
+			Assert.assertEquals(res3.jsonPath().getString("errorMessage"), "Dashboard with the same name exists already");
+			
+		}
+		catch (Exception e) {
+			Assert.fail(e.getLocalizedMessage());
+			LOGGER.info("context",e);
+		}
+	}
+	
+	@Test(dependsOnMethods = {"offBoardForTenant"})
+	public void verifyDataCleaned()
+	{
+		Response res4 = RestAssured
+				.given()
+				.contentType(ContentType.JSON)
+				.log()
+				.everything()
+				.headers("X-USER-IDENTITY-DOMAIN-NAME", tenantid, "X-REMOTE-USER", tenantid + "." + remoteuser,
+						"Authorization", authToken).when().get("/dashboards/" + dashboard_id);
+		
+		Assert.assertTrue(res4.getStatusCode() == 404);
+		Assert.assertEquals(res4.jsonPath().getString("errorCode"), "20001");
+		Assert.assertEquals(res4.jsonPath().getString("errorMessage"), "Specified dashboard is not found");
+	}
+	
 }
