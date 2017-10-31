34be2ff73e2ecbb7e4ff1b53f910d5e3ae6bb7ce
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-10-31 11:54:55 +0800

    EMCPDF-4970

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/WidgetSelectorUtil_1240.java
+++ dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/WidgetSelectorUtil_1240.java
@@ -45,47 +45,38 @@ public class WidgetSelectorUtil_1240 extends WidgetSelectorUtil_1230 {
         driver.getLogger().info("go into widget list popup");
         WebDriverWait webDriverWait = new WebDriverWait(driver.getWebDriver(), WaitUtil.WAIT_TIMEOUT);
 
-        WebElement searchInput = driver.getElement(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHINPUTCSS));
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(searchInput));
-        Actions actions = new Actions(driver.getWebDriver());
-        actions.moveToElement(searchInput).build().perform();
-        searchInput.clear();
-        WaitUtil.waitForPageFullyLoaded(driver);
-        actions.moveToElement(searchInput).build().perform();
+        driver.moveToElement(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHINPUTCSS));
+        driver.clear(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHINPUTCSS));
+        driver.moveToElement(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHINPUTCSS));
         driver.click(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHINPUTCSS));
-        searchInput.sendKeys(widgetName);
-        driver.takeScreenShot();
-        driver.savePageToFile();
-        Assert.assertEquals(searchInput.getAttribute("value"), widgetName);
-        WebElement searchButton = driver.getElement(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHBUTTONCSS));
-        driver.waitForElementPresent(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHBUTTONCSS));
-        searchButton.click();
+        driver.sendKeys(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHINPUTCSS), widgetName);
 
-        List<WebElement> matchingWidgets = driver.getWebDriver().findElements(By.cssSelector(DashBoardPageId_1220.RIGHTDRAWERWIDGETCSS));
-        if (matchingWidgets == null || matchingWidgets.isEmpty()) {
+        Assert.assertEquals(driver.getAttribute(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHINPUTCSS)+"@value"), widgetName);
+
+        driver.waitForElementPresent(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHBUTTONCSS));
+        driver.click(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERSEARCHBUTTONCSS));
+
+        WaitUtil.waitForPageFullyLoaded(driver);
+
+        driver.getLogger().info("Searching widget finished!!");
+
+        driver.getLogger().info("[DashboardHomeUtil] start to add widget from right drawer");
+
+        int widgetCount= driver.getElementCount(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERWIDGETCSS));
+        if (widgetCount <=0 ) {
             throw new NoSuchElementException("Right drawer widget for widget name =" + widgetName + " is not found");
         }
-
         WaitUtil.waitForPageFullyLoaded(driver);
-        Actions builder = new Actions(driver.getWebDriver());
-        try {
-            webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(DashBoardPageId_1220.RIGHTDRAWERWIDGETCSS)));
-            builder.moveToElement(driver.getWebDriver().findElement(By.cssSelector(DashBoardPageId_1220.RIGHTDRAWERWIDGETCSS)))
-                    .build().perform();
-        }catch (IllegalArgumentException e) {
-            throw new NoSuchElementException("Widget for " + widgetName + " is not found");
-        }
 
-        driver.getLogger().info("Focus to the widget");
-        driver.takeScreenShot();
-        driver.savePageToFile();
+        driver.waitForElementPresent(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERWIDGETCSS));
+
         driver.click(generateCssLocator(DashBoardPageId_1220.RIGHTDRAWERWIDGETCSS));
     }
 
     public void addWidgetInDialog(WebDriver driver, String widgetName){
         driver.getLogger().info("go into widget list dialog");
         searchWidget(driver, widgetName);
-        String autoCloseCheck = driver.getElement(DashBoardPageId.WIDGET_SELECTOR_WIDGET_AREA).getAttribute("data-wgt-slt-auto-close");
+        String autoCloseCheck = driver.getAttribute(DashBoardPageId.WIDGET_SELECTOR_WIDGET_AREA + "@data-wgt-slt-auto-close");
         Boolean autoClose = Boolean.valueOf(autoCloseCheck);
         getWidgetElementByTitle(driver, widgetName, 0);
         driver.click(DashBoardPageId.WIDGET_SELECTOR_OK_BTN_LOCATOR);
@@ -100,20 +91,23 @@ public class WidgetSelectorUtil_1240 extends WidgetSelectorUtil_1230 {
 
     }
 
-    public WebElement getWidgetElementByTitle(WebDriver driver, String widgetName, int index) {
+    public int getWidgetElementByTitle(WebDriver driver, String widgetName, int index)
+    {
         String xpath = XPathLiteral.getXPath(widgetName, driver.getLogger());
         String widgetItemByNameLocator = String.format(DashBoardPageId.WIDGET_SELECTOR_WIDGET_ITEMS_BY_TITLE, xpath);
 
         driver.click(widgetItemByNameLocator);
 
-        List<WebElement> tileTitles = driver.getWebDriver().findElements(By.xpath(widgetItemByNameLocator));
-        if (tileTitles == null || tileTitles.size() <= index) {
+        int tileTitlesCount = driver.getElementCount("xpath=" + widgetItemByNameLocator);
+
+        if (tileTitlesCount <= index) {
             throw new NoSuchElementException("Widget with widgetName=" + widgetName + ", index=" + index + " is not found");
         }
-        tileTitles.get(index).click();
-        driver.takeScreenShot();
-        driver.savePageToFile();
-        return tileTitles.get(index);
+
+        int i = index + 1;
+
+        driver.click("xpath=(" + widgetItemByNameLocator + ")[" + i + "]");
+        return i;
     }
 
     public void closeDialog(WebDriver driver){
