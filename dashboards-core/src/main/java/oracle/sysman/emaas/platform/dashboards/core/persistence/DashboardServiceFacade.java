6ba5536ece297068bc5dd5e3b925cbcb621e9896
kaishen <kaishen.ou@oracle.com>
2017-11-01 10:24:11 +0800

    emcpdf1542 revise the JPQL to support to find "%" debug－2

--- dashboards-core/src/main/java/oracle/sysman/emaas/platform/dashboards/core/persistence/DashboardServiceFacade.java
+++ dashboards-core/src/main/java/oracle/sysman/emaas/platform/dashboards/core/persistence/DashboardServiceFacade.java
@@ -127,8 +127,10 @@ public class DashboardServiceFacade
 		String jpql = "select d from EmsDashboard d where d.name LIKE :namePattern ESCAPE '\\' and d.owner = :owner and d.isSystem = 0 and d.deleted = 0 ";
 
 		namePattern = StringEscapeUtils.escapeHtml4(namePattern);
+		LOGGER.info("before change the string is " + namePattern);
 		if(namePattern.contains("%"))
 			namePattern = namePattern.replaceAll("%","\\\\\\\\%");
+		LOGGER.info("after change the string is " + namePattern);
 		List<EmsDashboard> list = em.createQuery(jpql,EmsDashboard.class)
 				.setParameter("namePattern", "%"+namePattern+"%")
 				.setParameter("owner", UserContext.getCurrentUser()).getResultList();
