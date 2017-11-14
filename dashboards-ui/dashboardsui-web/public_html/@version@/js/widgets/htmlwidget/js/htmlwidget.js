7586902f8dab8739ced4f092df3c5aa66d30bdf1
qiqia <qiqia>
2017-11-14 10:05:58 +0800

    add support for source code as a param in html widget

--- dashboards-ui/dashboardsui-web/public_html/@version@/js/widgets/htmlwidget/js/htmlwidget.js
+++ dashboards-ui/dashboardsui-web/public_html/@version@/js/widgets/htmlwidget/js/htmlwidget.js
@@ -1,14 +1,36 @@
 define(["require", "knockout", "jquery", "ojs/ojcore", 'DOMPurify'],
         function (localrequire, ko, $, oj, DOMPurify) {
+            function getGuid() {
+                function securedRandom(){
+                    var arr = new Uint32Array(1);
+                    var crypto = window.crypto || window.msCrypto;
+                    crypto.getRandomValues(arr);
+                    var result = arr[0] * Math.pow(2,-32);
+                    return result;
+                }
+                function S4() {
+                    return parseInt(((1 + securedRandom()) * 0x10000)).toString(16).substring(1);
+                }
+                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+            }
+
             function htmlWidgetViewModel(params) {
                 var self = this;
 
                 self.textAreaVal = ko.observable();
                 self.editing = ko.observable(false);
-                if(!params.tile.content) {
-                    params.tile.content = ko.observable();
+
+                if(params.tile){
+                    // branch for html editing on dashboard builder page with params.tile exists
+                    if(!params.tile.content) {
+                        params.tile.content = ko.observable();
+                    }
+                    self.content = params.tile.content;
+                }else{
+                    // branch for params.source without params.tile
+                    self.content = ko.observable(params.source);
                 }
-                self.content = params.tile.content;
+
                 self.emptyContent = ko.computed(function() {
                     if(!self.content()) {
                         return true;
@@ -21,15 +43,15 @@ define(["require", "knockout", "jquery", "ojs/ojcore", 'DOMPurify'],
                         return true;
                     }
                 });
-                self.htmlWidgetId = 'hahaha';
+                self.htmlWidgetId = params.tile?(params.tile.tileId ? ko.unwrap(params.tile.tileId) : params.tile.clientGuid):getGuid();
                 
                 var config = {
                     ALLOWED_TAGS: ['a','abbr','acronym','address','area','article','aside','b','bdi','bdo','big','blockquote','br','br','caption','center','cite','code','col','colgroup','dd','del','del','dfn','dir','div','dl','dt','em','figcaption','figure','font','footer','h1','h2','h3','h4','h5','h6','header','hgroup','hr','i','img','ins','kbd','label','li','map','map','mark','menu','nav','ol','p','pre','q','rp','rt','ruby','s','samp','section','small','span','strike','strong','sub','sup','table','tbody','td','tfoot','th','thead','time','tr','tt','u','ul','var','wbr'],
                     ALLOWED_ATTR: ['abbr','align','alt','axis','bgcolor','border','cellpadding','cellspacing','class','clear','color','cols','colspan','compact','coords','dir','face','headers','height','hreflang','hspace','ismap','lang','language','nohref','nowrap','rel','rev','rows','rowspan','rules','scope','scrolling','shape','size','span','start','summary','tabindex','target','title','type','valign','value','vspace','width','background','cite','href','src','style'],
                     ALLOW_DATA_ATTR: false
                 };
-                $("#textEditor").attr("id", "textEditor_" + self.htmlWidgetId);
-                $("#textEditor_" + self.htmlWidgetId).attr("contenteditable", "true");
+                $("#htmlEditor").attr("id", "htmlEditor_" + self.htmlWidgetId);
+                $("#htmlEditor_" + self.htmlWidgetId).attr("contenteditable", "true");
                 self.content(DOMPurify.sanitize(self.content(), config));
                 self.previewHTMLData = function(){
                     var rawHtml = self.textAreaVal();
@@ -40,21 +62,23 @@ define(["require", "knockout", "jquery", "ojs/ojcore", 'DOMPurify'],
 
                 self.showTextEditor = function(data, event) {
                     self.editing(true);
-                    $("#textEditor_" + self.htmlWidgetId).focus();
+                    $("#htmlEditor_" + self.htmlWidgetId).focus();
                 };
                 
                 self.editHTMLData = function(data, event) {
                     self.textAreaVal(self.content());
                     self.editing(true);
-                    $("#textEditor_" + self.htmlWidgetId).focus();
+                    $("#htmlEditor_" + self.htmlWidgetId).focus();
                 };
                 
-                params.tile.onDashboardItemChangeEvent = function(dashboardItemChangeEvent) {
-                    //When receive "Edit" event from dashboard, show html widget in edit mode
-                    if(dashboardItemChangeEvent && dashboardItemChangeEvent.tileChange && dashboardItemChangeEvent.tileChange.status === "POST_EDIT") {
-                        self.editHTMLData();
-                    }
-                };
+                if(params.tile){
+                    params.tile.onDashboardItemChangeEvent = function(dashboardItemChangeEvent) {
+                        //When receive "Edit" event from dashboard, show html widget in edit mode
+                        if(dashboardItemChangeEvent && dashboardItemChangeEvent.tileChange && dashboardItemChangeEvent.tileChange.status === "POST_EDIT") {
+                            self.editHTMLData();
+                        }
+                    };
+                }
             }
             return htmlWidgetViewModel;
         });
