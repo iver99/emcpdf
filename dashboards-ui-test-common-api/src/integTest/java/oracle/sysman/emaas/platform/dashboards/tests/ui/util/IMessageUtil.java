77bdc8ebd6394b410eda9c68bf68faf50ffc3581
Caleb Wei <caleb.wei@oracle.com>
2017-11-14 02:07:10 -0800

    add test and common api for message banner

---
+++ dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/util/IMessageUtil.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2017 Oracle
+ * All rights reserved.
+ *
+ * $$File: $$
+ * $$DateTime: $$
+ * $$Author: $$
+ * $$Revision: $$
+ */
+ 
+package oracle.sysman.emaas.platform.dashboards.tests.ui.util;
+
+/**
+ * @author cawei
+ *
+ */
+public class IMessageUtil
+{
+
+}
