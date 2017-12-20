0d2f918b91abdcb3b86c2b3dd269b43faf89c63c
chehao <chendong.h.hao@oracle.com>
2017-12-20 10:21:39 +0800

    extend name max length, bump up to 1.27.0

---
+++ dashboards-schema/src/main/sql/1.27.0/upgrade_impl_ddl.sql
@@ -0,0 +1,22 @@
+Rem --DDL change during upgrade
+Rem
+Rem upgrade_impl_ddl.sql
+Rem
+Rem Copyright (c) 2013, 2014, 2015, 2016 Oracle and/or its affiliates.
+Rem All rights reserved.
+Rem
+Rem    NAME
+Rem      upgrade_impl_ddl.sql
+Rem
+Rem    DESCRIPTION
+Rem      DDL change during upgrade
+Rem
+Rem    NOTES
+Rem      None
+Rem
+
+SET FEEDBACK ON
+SET SERVEROUTPUT ON
+@&EMSAAS_SQL_ROOT/1.27.0/emaas_dashboard_tile_title_length.sql
+
+
